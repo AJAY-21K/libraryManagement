23bdbd284b6b096b8cec9204d305690e7b644ecc
AJAY <abhishekajay212@gmail.com>
2024-10-27 19:32:02 +0530

    333

--- src/main/java/com/example/libraryManagement/CustomerService/CustomerService.java
+++ src/main/java/com/example/libraryManagement/CustomerService/CustomerService.java
@@ -1,4 +1,8 @@
 package com.example.libraryManagement.CustomerService;
 
+import com.example.libraryManagement.DTO.CustomerDTO;
+import com.example.libraryManagement.DTO.CustomerSaveDTO;
+
 public interface CustomerService {
+    String addCustomer(CustomerSaveDTO customerDTO);
 }
