66aec8c304607fc52026b492d449e5f4b838a932
AJAY <abhishekajay212@gmail.com>
2024-10-27 17:33:25 +0530

    2nd

--- src/main/java/com/example/libraryManagement/CustomerController/CustomerController.java
+++ src/main/java/com/example/libraryManagement/CustomerController/CustomerController.java
@@ -1,11 +1,23 @@
 package com.example.libraryManagement.CustomerController;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.libraryManagement.CustomerDTO.CustomerDTO;
+import com.example.libraryManagement.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping
+@RequestMapping("api/V1/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+
+    @PostMapping(path = "/save")
+    public String saveCustomer(@RequestBody CustomerDTO customerDTO){
+        String id = customerService.addCustomr(customerDTO);
+        re
+
+    }
+
 }
---
+++ src/main/java/com/example/libraryManagement/DTO/CustomerDTO.java
@@ -0,0 +1,4 @@
+package com.example.libraryManagement.DTO;
+
+public class CustomerDTO {
+}
