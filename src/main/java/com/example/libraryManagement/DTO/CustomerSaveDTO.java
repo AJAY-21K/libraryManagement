8b3a11e84c64268addb1dc621b64feaece6cfb6a
AJAY <abhishekajay212@gmail.com>
2024-10-27 19:29:14 +0530

    Save

---
+++ src/main/java/com/example/libraryManagement/DTO/CustomerSaveDTO.java
@@ -0,0 +1,58 @@
+package com.example.libraryManagement.DTO;
+
+public class CustomerSaveDTO {
+    private String customerName;
+    private String customerAddress;
+    private String mobile;
+
+
+    public CustomerSaveDTO(String customerName, String customerAddress, String mobile) {
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.mobile = mobile;
+    }
+
+    public CustomerSaveDTO() {
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", mobile='" + mobile + '\'' +
+                '}';
+    }
+}
